86cc2a784b8fa8ca303e2851e3852c9983b44320
Ma1y0 <matyas.barr@gmail.com>
2023-03-05 21:57:38 +0100

    Draw option and switch to svg icons

--- src/Components/Cell.tsx
+++ src/Components/Cell.tsx
@@ -4,6 +4,19 @@ type propsType = {
 	value: "X" | "O" | ""
 }
 
-const Cell: Component = (props: propsType) => <p class="font-semibold text-4xl">{props.value}</p>
+const Cell: Component = (props: propsType) => (
+	<div class="h-16 w-16 flex items-center justify-center">
+		<svg viewBox="0 0 100 100" class="h-16 w-16">
+			{props.value === "X" ? (
+				<>
+					<line x1="20" y1="20" x2="80" y2="80" stroke="#f92f60" stroke-width="8" />
+					<line x1="20" y1="80" x2="80" y2="20" stroke="#f92f60" stroke-width="8" />
+				</>
+			) : props.value === "O" ? (
+				<circle cx="50" cy="50" r="40" stroke="#0074ba" stroke-width="8" fill="none" />
+			) : null}
+		</svg>
+	</div>
+)
 
 export default Cell
--- src/Game.tsx
+++ src/Game.tsx
@@ -2,7 +2,7 @@ import { type Component, createSignal, createEffect } from "solid-js"
 import Cell from "./Components/Cell"
 
 type GridType = "X" | "O" | ""
-type WhoWonType = "X" | "O" | "" | null
+type WhoWonType = "X" | "O" | "Draw" | "" | null
 
 const Game: Component = () => {
 	const [grid, setGrid] = createSignal<GridType[]>(["", "", "", "", "", "", "", "", ""])
@@ -36,10 +36,21 @@ const Game: Component = () => {
 		// Check diagonals
 		if (a[0] !== "" && a[0] === a[4] && a[0] === a[8]) {
 			setWinner(a[0])
-			console.log(a[0])
 		}
 		if (a[2] !== "" && a[2] === a[4] && a[2] === a[6]) {
 			setWinner(a[2])
+		}
+
+		// Check draw
+		let hasBlanks: boolean
+		for (let i = 0; i < 9; i++) {
+			if (a[i] == "") {
+				hasBlanks = true
+			}
+		}
+
+		if (!hasBlanks && winner() === null) {
+			setWinner("Draw")
 		}
 	}
 
@@ -54,27 +65,33 @@ const Game: Component = () => {
 	return (
 		<main class="h-screen">
 			<div class="absolute min-w-full flex justify-center items-center top-10">
-				{winner() === "O" || winner() === "X" ? <h1>{winner()} Won</h1> : null}
+				{winner() === "O" || winner() === "X" || winner() === "Draw" ? (
+					<h1>
+						{winner()} {winner() !== "Draw" ? "Won" : null}
+					</h1>
+				) : null}
 			</div>
-			<div class="flex justify-center items-center flex-col min-h-full">
-				{[0, 3, 6].map((startIndex) => (
-					<div class="flex">
-						{grid()
-							.slice(startIndex, startIndex + 3)
-							.map((value, index) => (
-								<div
-									onClick={() => {
-										handleClick(startIndex + index)
-									}}
-									class="p-1 border w-20 h-20 hover:cursor-pointer flex justify-center items-center"
-								>
-									<Cell key={startIndex + index} value={value} />
-								</div>
-							))}
-					</div>
-				))}
+			<div class="flex justify-center items-center flex-col min-h-full min-w-full">
+				<div>
+					{[0, 3, 6].map((startIndex) => (
+						<div class="flex">
+							{grid()
+								.slice(startIndex, startIndex + 3)
+								.map((value, index) => (
+									<div
+										onClick={() => {
+											handleClick(startIndex + index)
+										}}
+										class="p-1 border w-20 h-20 hover:cursor-pointer flex justify-center items-center"
+									>
+										<Cell key={startIndex + index} value={value} />
+									</div>
+								))}
+						</div>
+					))}
+				</div>
 				<div class="m-6">
-					{winner() === "O" || winner() === "X" ? (
+					{winner() === "O" || winner() === "X" || winner() === "Draw" ? (
 						<button onClick={handleRestart}>Play again?</button>
 					) : null}
 				</div>
